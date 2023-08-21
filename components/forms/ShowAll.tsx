8e6231a22e9c8cc6090931f064c5fe4337dd8426
Jonas <jonas.m.a.1997@gmail.com>
2023-08-21 08:25:15 -0300

    add show bucket on all

--- components/forms/ShowAll.tsx
+++ components/forms/ShowAll.tsx
@@ -1,12 +1,21 @@
 "use client";
 import ImageForm from "@/components/forms/ImageForm";
 import axios from "axios";
+import { useState } from "react";
 
 export default function ShowAll() {
+  const [bucket, setBucket] = useState<any>();
+
   const showAll = () => {
-    axios.get("http://localhost:5000/listObjects").then((res) => {
-      console.log(res);
-    });
+    axios
+      .get("http://localhost:5000/listObjects")
+      .then((res) => {
+        console.log(res);
+        setBucket(res.data["Images"]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <>
@@ -17,6 +26,19 @@ export default function ShowAll() {
         >
           Listar
         </button>
+      </div>
+      <div className="m-4 flex items-center flex-wrap gap-4">
+        {bucket?.map((el: any) => (
+          <div key={el}>
+            <img
+              height={280}
+              width={280}
+              src={`data:image/jpg;base64,${el}`}
+              alt="image"
+            />
+            <p></p>
+          </div>
+        ))}
       </div>
     </>
   );
